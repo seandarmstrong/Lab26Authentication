07bf481b166d52c5a7cf4656461e879d9ae1b59d
sean <armstrong.d.sean@gmail.com>
2018-08-28 20:18:28 -0400

    first commit of project

---
+++ UserAuthenticationLab/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UserAuthenticationLab.Startup))]
+namespace UserAuthenticationLab
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
