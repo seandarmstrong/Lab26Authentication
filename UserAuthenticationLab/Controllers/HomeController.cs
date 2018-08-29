319d64f608688635323d18cba7204e7ba9c0964c
sean <armstrong.d.sean@gmail.com>
2018-08-28 21:04:39 -0400

    set up a page that only be viewed when logged in

--- UserAuthenticationLab/Controllers/HomeController.cs
+++ UserAuthenticationLab/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace UserAuthenticationLab.Controllers
 {
@@ -24,6 +20,12 @@ namespace UserAuthenticationLab.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult TechnicalInterviews()
+        {
             return View();
         }
     }
